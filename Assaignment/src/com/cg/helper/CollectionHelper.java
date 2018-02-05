6338a894d45ee4b1285a96143557175d6db0e6d0
Tamonash <35283282+Tamonash@users.noreply.github.com>
2018-02-05 14:28:31 +0530

    Add files via upload

---
+++ Assaignment/src/com/cg/helper/CollectionHelper.java
@@ -0,0 +1,16 @@
+package com.cg.helper;
+
+public class CollectionHelper {
+	public void addID(int id){
+		
+	}
+	public void addName(String name){
+		
+	}
+	public void addPrice(double price){
+		
+	}
+	public void addTxnID(double txnID){
+		
+	}
+}
---
+++ Assaignment/src/com/cg/helper/Validator.java
@@ -0,0 +1,14 @@
+package com.cg.helper;
+
+public class Validator {
+	public static boolean validateID(String id){
+		return true;
+	}
+	public static boolean validateName(String name){
+		return true;
+	}
+	public static boolean validatePrice(String price){
+		return true;
+	}
+
+}
